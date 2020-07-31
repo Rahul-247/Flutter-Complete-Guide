db14682a85474cab89038099d1317c42adcc0436
rbehera <rbehera@asu.edu>
2020-07-31 17:28:29 +0530

    Begning of Quiz App Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen');
+  }
+
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s you favorite color?',
+      'What\s you favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("First App"),
+          title: Text("My First App"),
         ),
         body: Center(
-          child: Text(
-            "Hello! World", 
-            ),
+          child: Column(
+            children: [
+              Text(
+                questions[0],
+              ),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          ),
         ),
       ),
     );
